9b0aa05fd606afee944bcc0bc2f71ebd220b6a91
Build-coder <59242259+Build-coder@users.noreply.github.com>
2020-02-06 14:19:00 -0500

    Update lab01.cpp

--- lab01.cpp
+++ lab01.cpp
@@ -1,100 +1,40 @@
-#include <stdio.h>
 #include <iostream>
 #include <string>
-
 using namespace std;
-
-char check_grade(float grade)
-{
-    bool valid = true;
-    
-    if(grade > 0 && grade < 100)
-        valid = true;
-    else
-        valid = false;
-
-    return valid;
-}
-
-char invalid_input(float grade)
-{
-    bool invalid = true;
-    
-    while(invalid)
-    {
-        cout << "Invalid input. Enter a grade between 0 and 100: ";
-        cin >> grade;
-        invalid = check_grade(grade);
-        invalid = !invalid;
-    }
-    
-    return grade;
-}
-
-char check_entries(int entries)
-{
-    bool limit = false;
-    
-    if(entries > 2)
-        limit = true;
-    else
-        limit = false;
-        
-    return limit;
-}
-
-
 
 int main()
 {
-
-    char first_name[15];
-    char last_name[15];
-
-    float grade = 0;
-    float sum = 0;
+    string name;
+    float first_grade; 
+    float second_grade;
+    float third_grade;
     
-    bool valid = true;
-    bool limit = false;
-    int entries = 0;
-
-    cout << "Enter your first name: " << endl;
-    cin.getline(first_name, 10);
-    cout << "Enter your last name: " << endl;
-    cin.getline(last_name, 10); 
-
-    while(valid, limit==false)
+    int total_attempts = 0;
+    int record_number = 0;
+    
+    cout << "Enter your first and last name: " << endl;
+    getline(cin, name);
+    
+    cout << "Please enter your first grade: " << endl;
+    cin >> first_grade;
+    
+    total_attempts = total_attempts + 1;
+    record_number = record_number + 1;
+    
+    while(cin.fail())
     {
-        cout << "Enter your grade: " << endl;
-        cin >> grade;
-        
-        valid = check_grade(grade);
-           
-        if(valid)
-        {
-            limit = check_entries(entries);
-            
-            if(valid, limit==false)
-            {    
-                sum = sum + grade;
-                entries = entries + 1;
-            }
-        }
-        else
-        {
-            grade = invalid_input(grade);
-            limit = check_entries(entries);
-            
-            if(limit==false)
-            {
-                sum = sum + grade;
-                entries = entries + 1;
-            }
-        }
-    }        
+        cout << "Invalid input. Enter a number: " << endl;
+        cin.clear();
+        cin.ignore(256, '\n');
+        cin >> first_grade;
+        total_attempts = total_attempts + 1;
+    }
     
-    cout << "Your full name is: " << first_name << " " << last_name << endl;
-    cout << "Your grade average is: " << sum << endl;
+    cout << "Your first grade is valid!" << endl;
+    cout << "The total attempt times are: " << total_attempts << endl;
+    cout << "The valid records number is: " << record_number << endl;
+    
+    cout << "Your name is: " << name << endl;
 
     return 0;
 }
