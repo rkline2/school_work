4232f41fb038456bc2687cc82624436cc11bd9af
Build-coder <59242259+Build-coder@users.noreply.github.com>
2020-02-09 14:17:13 -0500

    Update lab01.cpp

--- lab01.cpp
+++ lab01.cpp
@@ -4,71 +4,81 @@ using namespace std;
 
 float get_grade(float grade)
 {
-        int total_attempts = 0;
-        bool valid = true;   
+    int total_attempts = 0;
+    bool valid = true;   
 
-        cout << "Enter a grade between 0 - 100: ";
+    cout << "Enter a grade between 0 - 100: ";
+    cin >> grade;
+
+    total_attempts += 1;
+
+    if(grade < 0 || grade > 100)
+        valid = false;
+		
+ while(cin.fail() || valid == false)
+    {
+        cout << "Invalid input. Enter a grade between 0 - 100: ";
+        cin.clear();
+        cin.ignore(256, '\n');
         cin >> grade;
 
         total_attempts += 1;
 
         if(grade < 0 || grade > 100)
-            valid = false;
+                valid = false;
+        else
+                valid = true;
+    }
+           
+    cout << "Your grade is valid!" << endl;
+    cout << "The total attempt times are: " << total_attempts << endl;
 
-        while(cin.fail() || valid == false)
-        {
-            cout << "Invalid input. Enter a grade between 0 - 100: ";
-            cin.clear();
-            cin.ignore(256, '\n');
-            cin >> grade;
-
-            total_attempts += 1;
-
-            if(grade < 0 || grade > 100)
-                    valid = false;
-            else
-                    valid = true;
-        }
-
-        cout << "Your grade is valid!" << endl;
-        cout << "The total attempt times are: " << total_attempts << endl;
-
-        return grade;
+    return grade;
 }
 
 int main()
 {
-       string name;
-       int sum = 0;
-       int record_number = 0;
+    string name;
+    
+    int record_number = 0;
 
-       float first_grade = 0.0;
-       float second_grade = 0.0;
-       float third_grade = 0.0;
+    float first_grade = 0.0;
+    float second_grade = 0.0;
+    float third_grade = 0.0;
 
-       float average = 0.0;
+    float average = 0.0;
 
-       cout << "Enter your first and last name: ";
-       getline(cin, name);
+    //char first_name[80];
+    //char last_name[80];
 
-       first_grade = get_grade(first_grade);
-       record_number += 1; 
-       cout << "The valid records number is: " << record_number << endl;
-       cout << " " << endl;   
+    //cout << "Enter your first name: ";
+    //cin.getline(first_name, 80);
 
-       second_grade = get_grade(second_grade);
-       record_number += 1; 
-       cout << "The valid records number is: " << record_number << endl;
-       cout << " " << endl;  
+    //cout << "Enter your last name: ";
+    //cin.getline(last_name, 80);
 
-       third_grade = get_grade(third_grade);
-       record_number += 1; 
-       cout << "The valid records number is: " << record_number << endl;
-       cout << " " << endl;  
+    cout << "Enter your first and last name: ";
+    getline(cin, name);
 
-       average = (first_grade + second_grade + third_grade)/record_number;
-
-       cout << name << " your average is: " << average << endl;
-
-       return 0;
+    first_grade = get_grade(first_grade);
+    record_number += 1; 
+    cout << "The valid records number is: " << record_number << endl;
+    cout << " " << endl;   
+ 
+    second_grade = get_grade(second_grade);
+    record_number += 1; 
+    cout << "The valid records number is: " << record_number << endl;
+    cout << " " << endl;  
+ 
+    third_grade = get_grade(third_grade);
+    record_number += 1; 
+    cout << "The valid records number is: " << record_number << endl;
+    cout << " " << endl;  
+    
+    average = (first_grade + second_grade + third_grade)/record_number;
+ 
+    cout << name << " your average is: " << average << endl;
+    //cout << first_name << " " << last_name << " your average is: " << average << endl;
+      
+    return 0;
 }
