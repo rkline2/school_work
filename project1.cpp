7ea3632b7d1bd0e5ce288f38e811ed5b7637f8f2
Build-coder <59242259+Build-coder@users.noreply.github.com>
2020-02-15 20:51:08 -0500

    Update project1.cpp

--- project1.cpp
+++ project1.cpp
@@ -9,13 +9,25 @@ using namespace std;
 int checkRow(int i, int row)
 {
         //creates new row after 4 cols created
-        if(i!=0 && i%4==0)
+        if(i%4==0)
         {
                 row++;
                 cout << endl << row << "|  ";
         }
 
         return row;
+}
+
+
+int checkCol(int i, int col)
+{
+        //creates new row after 4 cols created
+        if(i%4==0)
+                col = 1;
+        else
+                col++;
+
+        return col; 
 }
 
 void printBoard(char testFile[]) 
@@ -26,7 +38,8 @@ void printBoard(char testFile[])
         //open file 
         inputStream.open(testFile);
 
-        int i = 0, row = 1, num = 0;
+        int i = 0, row = 1, col = 1, num = 0;
+        int pointZero[2] = {0,0};
 
         cout << "     1    2    3    4" << endl << row << "|  ";
 
@@ -35,21 +48,29 @@ void printBoard(char testFile[])
                 //read line/num from file
                 inputStream >> num;
 
-                if(num>9)
+                if(num==0)
                 {
+                        pointZero[0] = row;
+                        pointZero[1] = col;
+
                         row = checkRow(i, row);
+                        col = checkCol(i, col);
+                        i++;
+                } else if(num>9) {
+                        row = checkRow(i, row);
+                        col = checkCol(i, col);
                         cout << num << "|  ";
                         i++;
-                }
-                else
-                {
+                } else {
                         row = checkRow(i, row);
+                        col = checkCol(i, col);
                         cout << " " << num << "|  ";
                         i++;
                 }
         }
 
         cout << endl << endl;
+        cout << "Coordinates of Zero are: (" << pointZero[0] << "," << pointZero[1] << ")" << endl;
 }
 
 int main() 
