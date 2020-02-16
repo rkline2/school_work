c649f26560563987a75390596ff67f6dcd6ed7eb
Build-coder <59242259+Build-coder@users.noreply.github.com>
2020-02-15 20:04:56 -0500

    Update project1.cpp

--- project1.cpp
+++ project1.cpp
@@ -7,10 +7,11 @@
 
 using namespace std;
 
+
 int checkRow(int i, int row)
 {
         //creates new row after 4 cols created
-        if(i%4==0)
+        if(i!=0 && i%4==0)
         {
                 row++;
                 cout << endl << row << "|  ";
@@ -19,7 +20,7 @@ int checkRow(int i, int row)
         return row;
 }
 
-void printBoard(char testFile[], char line[])
+void printBoard(char testFile[]) 
 {
         //creation of ifstream class object to read file
         ifstream inputStream;
@@ -27,26 +28,27 @@ void printBoard(char testFile[], char line[])
         //open file 
         inputStream.open(testFile);
 
-        int i = 0, row = 1;
-        int num = 0;
+        int i = 0, row = 1, num = 0;
 
-        cout << "    1   2   3   4" << endl << row << "|  ";
+        cout << "     1    2    3    4" << endl << row << "|  ";
 
-        while(i<15)
+        while(i<16)
         {
                 //read line/num from file
                 inputStream >> num;
 
                 if(num>9)
                 {
-                        cout << num << " |  ";
                         row = checkRow(i, row);
+                        cout << num << "|  ";
                         i++;
                 }
                 else
-                        cout << num << "| ";
+                {
                         row = checkRow(i, row);
+                        cout << " " << num << "|  ";
                         i++;
+                }
         }
 
         cout << endl << endl;
@@ -55,13 +57,13 @@ void printBoard(char testFile[], char line[])
 
 int main() 
 {
-        char test1[15], test2[15], test3[15], testFile[15], line[4];
+        char test1[15], test2[15], test3[15], testFile[15];
 
         cout << "Enter test file: ";
         cin >> testFile;
         cout << "Test file chosen: " << testFile << endl << endl;
 
-        printBoard(testFile, line);
+        printBoard(testFile);
 
         return 0;
 }
